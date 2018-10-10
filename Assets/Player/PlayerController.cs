920b5b9db68096f312d479c6f37da6ad92dbe22e
OMARV\Omar <mohammad.omar.317@gmail.com>
2018-10-10 21:33:35 +0800

    Added Player movement, rotation and camera rotation mechanics in player controller and motor script.

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(PlayerHealth))]
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(PlayerSetup))]
+[RequireComponent(typeof(PlayerShoot))]
+public class PlayerController : NetworkBehaviour
+{
+
+    PlayerHealth m_playerHealth;
+    PlayerMotor m_playerMotor;
+    PlayerSetup m_playerSetup;
+    PlayerShoot m_playerShoot;
+
+    private GameObject m_cameraPivot;
+
+    public override void OnStartLocalPlayer()
+    {
+        GameObject.Find("MainCamera").GetComponent<Camera>().enabled = false;
+        GetComponentInChildren<Camera>().enabled = true;
+        m_cameraPivot = GameObject.Find("PlayerCameraPivot");
+    }
+
+    void Start ()
+    {
+        m_playerHealth = GetComponent<PlayerHealth>();
+        m_playerMotor = GetComponent<PlayerMotor>();
+        m_playerSetup = GetComponent<PlayerSetup>();
+        m_playerShoot = GetComponent<PlayerShoot>();
+	}
+	
+	void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        m_playerMotor.RotateCamera();
+	}
+
+    void FixedUpdate ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        m_playerMotor.MovePlayer(GetInput());
+        m_playerMotor.RotatePlayer(GetInput());
+    }
+
+    Vector3 GetInput ()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        return new Vector3( v, h, 0);
+    }
+}
---
+++ Assets/Player/PlayerMotor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : NetworkBehaviour
+{
+
+    Rigidbody m_rigidbody;
+
+    public float turnSpeed = 2f;
+    public float moveSpeed = 35f;
+
+    public float m_cameraTurnSpeed = 1f;
+
+    private GameObject m_cameraPivot;
+
+    void Start ()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_cameraPivot = GameObject.Find("PlayerCameraPivot");
+    }
+
+    public void MovePlayer(Vector3 input)
+    {
+        m_rigidbody.AddTorque ( 0, input.y * turnSpeed * Time.fixedDeltaTime, 0f);
+    }
+
+    public void RotatePlayer(Vector3 input)
+    {
+        m_rigidbody.AddForce(transform.right * input.x * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public void RotateCamera()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            m_cameraPivot.transform.Rotate(Vector3.up * m_cameraTurnSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            m_cameraPivot.transform.Rotate(Vector3.up * -m_cameraTurnSpeed * Time.deltaTime);
+        }
+    }
+}
